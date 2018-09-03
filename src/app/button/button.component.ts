12e18f6c764c77a7e81e18aac34deab3b745cf4e
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-09-03 10:25:12 +0800

    init

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'custom-button',
-  template: `
-    <p>
-      button works!
-    </p>
-  `,
-  styles: [],
+  template: `<button (click)="handleClick()">{{label}}</button>`,
+  styles: [`
+    button {
+      border: solid 3px;
+      padding: 8px 10px;
+      background: #bada55;
+      font-size: 20px;
+    }
+  `],
   encapsulation: ViewEncapsulation.Native
 })
 export class ButtonComponent implements OnInit {
 
+  @Input() label = 'default label';
+  @Output() action = new EventEmitter<number>();
+  private clicksCt = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleClick() {
+    this.clicksCt++;
+    this.action.emit(this.clicksCt);
   }
 
 }
